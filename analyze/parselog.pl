d8a475dd748bb1212c263606497136c2e4ef85a6
pixeldexter <ekilachkoff@gmail.com>
2012-12-02 06:20:36 +0400

    handle negative values properly

--- analyze/parselog.pl
+++ analyze/parselog.pl
@@ -9,6 +9,7 @@ use Data::Dumper;
 my $config = {
 	      pagesize => 64,
 	      maxpages => 96,
+	      sensors => 1,
 };
 
 GetOptions($config,
@@ -16,6 +17,7 @@ GetOptions($config,
 	   'output=s',
 	   'pagesize=i',
 	   'maxpages=i',
+	   'sensors=i',
 	  ) or pod2usage(255);
 
 die "--input missing" unless exists $config->{input};
@@ -25,17 +27,22 @@ binmode $fh, ":bytes";
 
 my $pagecount = 0;
 while ( read($fh, my $page, $config->{pagesize}) ) {
-#  my @bytes = pack 'C*', (split(//, $page));
-  my @bytes = split //, $page;
 
   my $offset = $config->{pagesize} / 2 / 8;
   while ( $offset < $config->{pagesize} ) {
-    my $time = ord($bytes[$offset + 0]) + (ord($bytes[$offset + 1]) << 8);
-    my $data = ord($bytes[$offset + 2]) + (ord($bytes[$offset + 3]) << 8);
-    $offset += 4;
-
+    # read time
+    my $time = unpack("S", substr($page, $offset, 2));
+    $offset += 2;
     last if $time == 65535;
-    printf "%d,%f\n", $time, $data / 16.0;
+    printf "%d", $time;
+
+    # read samples
+    for(my $i=0; $i!=$config->{sensors}; ++$i) {
+      my $data = unpack("s", substr($page, $offset, 2));
+      $offset += 2;
+      printf ",%f", $data / 16.0;
+    }
+    printf "\n";
   }
   $pagecount++;
   last if $pagecount ge $config->{maxpages};
